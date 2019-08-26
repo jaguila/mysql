2d002cb6f1c24811803d6574e62e18b17489bbb2
Dexter Aguila <aguila.dexter@gmail.com>
2019-08-25 21:12:39 -0700

    n

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+db=SQLAlchemy(app)
+class Comment(db.Model):
+    __tablename__="comments"
+    id=db.Column(db.Integer, primary_key=True)
+    content=db.Column(db.String(4096))
+comments=[]
 
-@app.route('/')
-def hello_world():
-    return render_template('mainpage.html')
+
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="patupe",
+    password="paSSwoRd1986",
+    hostname="patupe.mysql.pythonanywhere-services.com",
+    databasename="patupe$comments"
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    if request.method=='GET':
+        return render_template('mainpage.html', comments=comments)
+    comments.append(request.form['contents'])
+    return redirect(url_for('index'))
+
 if __name__=='__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
