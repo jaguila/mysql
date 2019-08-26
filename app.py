c647dc4a0157fc9c9a91db24c5b87c48bc9959a3
Dexter Aguila <aguila.dexter@gmail.com>
2019-08-25 21:57:50 -0700

    n

--- app.py
+++ app.py
@@ -26,8 +26,11 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 @app.route('/', methods=["GET","POST"])
 def index():
     if request.method=='GET':
-        return render_template('mainpage.html', comments=comments)
-    comments.append(request.form['contents'])
+        return render_template('mainpage.html', comments=Comment.query.all())
+    # comments.append(request.form['contents'])
+    comment=Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__=='__main__':
